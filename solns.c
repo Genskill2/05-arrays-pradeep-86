3135bf6a9f0bb54ee15a39cffaa1ace3cf8ff33e
pradeep-86 <84574355+pradeep-86@users.noreply.github.com>
2021-06-18 20:50:40 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -1,93 +1,97 @@
 /* Enter your solutions in this file */
-#include<stdio.h>
+#include <stdio.h>
 
-int max(int x[],int y)
-{
-       int large=x[0];
-       for(int i=1;i<y;i++)
-       {
-          if(x[i]>large)
-          large =x[i];
-       }
-       return large;
+int max(const int arr[],int n){
+	int Max=arr[0];
+	for(int i=0;i<n;i++){
+		if(arr[i]>Max){
+			Max=arr[i];
+		}
+	}
+	return Max;
 }
 
-int min(int x[],int y)
-{
-       int small=x[0];
-       for(int i=1;i<y;i++)
-       {
-          if(x[i]<small)
-          small=x[i];
-       }
-       return small;
+int min(const int arr[],int n){
+	int Min=arr[0];
+	for(int i=0;i<n;i++){
+		if(arr[i]<Min){
+			Min=arr[i];
+		}
+	}
+	return Min;
 }
 
-float average(int x[],int y)
-{
-       float sum=0;
-       for(int i=0;i<y;i++)
-       {
-          sum+=x[i];
-       }
-       return sum/y;
+float average(const int arr[],int n){
+	float sum=0.0;
+	for(int i=0;i<n;i++){
+		sum+=arr[i];
+	}
+	return sum/(float)n;
 }
 
 
-int mode(int x[],int y)
-{
-       int tol_max=0,max_val;
-       for(int i=0;i<y;i++)
-       {
-          int max=0;
-          for(int j=0;j<y;j++)
-          {
-             if(x[j]==x[i])
-             max++;
-          }
-          if(max>tol_max)
-          {
-            tol_max=max;
-            max_val=x[i];
-          }
-       }
-       return max_val;
+int mode(int arr[],int n){
+	int num,temp_num;
+	int count=0,temp_count=0;
+	// sort(arr,n);
+	int temp;
+	for(int i=0;i<n-1;i++){
+		for(int j=0;j<n-1;j++){
+			if(arr[j+1]<arr[j]){
+				temp=arr[j];
+				arr[j]=arr[j+1];
+				arr[j+1]=temp;
+			}
+		}
+	}
+	temp_num=arr[0];
+	for(int i=0;i<n;i++){
+		if(arr[i]==temp_num){
+			temp_count++;
+		}
+		else{
+			if(temp_count>count){
+				count=temp_count;
+				num=temp_num;
+			}
+			temp_num=arr[i];
+			temp_count=1;
+		}
+	}
+	if(temp_count>count){
+		count=temp_count;
+		num=temp_num;
+	}
+	return num;
 }
 
-int factors(int x,int y[])
-{
-       int k=0;
-       int a=x;
-       while(a%2==0)
-       {
-         a/=2;
-         y[k]=2;
-         k++;
-       }
-       while(a>3)
-       {
-          for(int i=3;i<x/2;i++)
-          {
-             if(a%i==0)
-             {
-               int count=0;
-               for(int j=2;j<i;j++)
-               {
-                  if(i%j==0)
-                  {
-                    count++;
-                    break;
-                  }
-               }
-               if(count==0)
-               {
-                 y[k]=i;              
-                 k++;
-                 a/=i;
-                 break;
-               }
-             }
-          }
-       }
-       return k;
+
+
+int factors(int num,int arr[]){
+	int prime=2,ref=0;
+	int count=0,index=0;
+	while(num!=1){
+		if(num%prime==0){
+			num=num/prime;
+			count++;
+			arr[index]=prime;
+			index++;
+		}
+		else{
+			while(1){
+				prime++;
+				for(int j=2;j<prime;j++){
+					if(prime%j==0)
+						ref++;
+				}
+				if(ref==0){
+					break;
+				}
+				else{
+					ref=0;
+				}
+			}
+		}
+	}
+	return count;
 }
