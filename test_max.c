b0a60ee5f4e0b9d70d2a1e9404658341834731bf
pradeep-86 <84574355+pradeep-86@users.noreply.github.com>
2021-06-18 20:57:04 +0530

    Update test_max.c

--- test_max.c
+++ test_max.c
@@ -2,13 +2,13 @@
 #include <assert.h>
 
 
-float average(int [], int);
+int max(int [], int);
 
 int main(void) {
   int x[] = {9,5,6,10,2,-3,4};
-  assert ((average(x, 7) - 4.7142) < 0.001);
+  assert (max(x, 7) == 10);
 
   int y[] = {5};
-  assert (average(y, 1) == 5.0);
-  printf("Average: passed\n");
+  assert (max(y, 1) == 5);
+  printf("Max: passed\n");
   }
